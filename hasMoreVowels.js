3369ff15e2379114d7ff4840799c58458e88312e
Garrett Pace <elderpace6@gmail.com>
2023-04-25 10:50:38 -0600

    4th

--- hasMoreVowels.js
+++ hasMoreVowels.js
@@ -39,27 +39,63 @@ console.log(switcheroo(array))
 // hasMoreVowels('Aal')
 // // true
 
-word = word.toLowerCase
+function hasMoreVowels(word) {
 
-let vowelCount = 0
-let nonVowelCount = 0
-for (let i = 0; i < word.length; i++) {
-    let curChar = word [i]
-    if (curChar === 'a' ||
-    curChar === 'e' ||
-    curChar === 'i' ||
-    curChar === 'o' ||
-    curChar === 'u'
-    ) {
-        vowelCount++
+    //what are vowels? the characters:
+    // a e i o u
+    // and sometimes y, but we're going to not consider it now
+
+    //wait a sec. they can pass in uppercase and lowercase letters
+    //which means I want to switch them all to lowercase so
+    //my code works more simply
+    word = word.toLowerCase()
+
+    //to see if 'word' has more vowels
+    //we're going to keep track of the amount of vowels,
+    //and the amount of non-vowels.
+    //we'll make variables set to 0, since the count starts
+    //at 0
+    let vowelCount = 0
+    let nonVowelCount = 0
+
+    // loop through each character of 'word'
+    // at look at each character as you encounter it
+    for (let i = 0; i < word.length; i++) {
+
+        // if the character is 'a' or 'e' or 'i' or 'o' or 'u'
+        // then it is a vowel: increase the vowel count
+        let curChar = word[i]
+
+        if (
+            curChar === 'a' ||
+            curChar === 'e' ||
+            curChar === 'i' ||
+            curChar === 'o' ||
+            curChar === 'u'
+        ) {
+            vowelCount++
+
+        // if it is not one of those vowels,
+        // then it is not a vowel: increase the non-vowel count
+        } else {
+            nonVowelCount++
+        }
+    }
+
+    //once you've finished looking at all of the characters
+    //check and see if the vowel count is greater than the
+    //non-vowel count.
+    if (vowelCount > nonVowelCount) {
+        //if it is, return true
+        return true
     } else {
-        nonVowelCount++
+        //if it is not, return false
+        return false
     }
-    }
-if (vowelCount > nonVowelCount) {
-    return true
-} else {
-return false
 }
 
-console.log(hasMoreVowels('monday'))
+console.log(hasMoreVowels('monday')) //false
+console.log(hasMoreVowels('EEL')) //true
+console.log(hasMoreVowels('eel')) //true
+console.log(hasMoreVowels('mississippi')) //false
+console.log(hasMoreVowels('Beazel')) //false
